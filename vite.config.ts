ff8892bf07ab4c5dd2bd9ac2abef1be9b5ee670a
Julia <veluat@gmail.com>
2025-06-02 20:23:15 +0300

    chore: update configs and settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
-import { defineConfig } from 'vite';
-import path from 'path';
+import {defineConfig} from 'vite'
+import path from 'path'
 
 export default defineConfig({
+  root: './src',
+  base: '/',
+  publicDir: '../public',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@core': path.resolve(__dirname, './src/core'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+    extensions: ['.ts', '.js', '.scss', '.svg']
+  },
+  optimizeDeps: {
+    include: ['imask'],
+    exclude: ['vite-plugin-svg-icons']
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+      },
     },
   },
-});
+  server: {
+    port: 3000,
+    open: true
+  }
+})
